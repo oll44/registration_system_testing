072f12b99f2242ac159ab18d87ce80073f725ec5
Eli250 <hirwaeli@outlook.com>
2022-06-03 01:22:10 +0200

    added a line

--- src/routes/api/user.route.js
+++ src/routes/api/user.route.js
@@ -5,4 +5,7 @@ const userRoutes = express.Router();
 userRoutes.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to user routes" });
 });
+userRoutes.get("/:id", (req, res) => {
+  res.status(200).json({ message: "Specific user" });
+});
 export default userRoutes;
